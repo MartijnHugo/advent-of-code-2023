b60c27376dbd64ac154366133183e6ff68142a9a
MartijnHugo <martijnhdejong@gmail.com>
2023-12-08 20:14:52 +0100

    Trying to map ranges

--- src/5.cpp
+++ src/5.cpp
@@ -155,47 +155,42 @@ const std::string INPUT_FILE = "etc/5.txt";
 class Range
 {
 public:
-    Range(uint32_t start, uint32_t length) : start_(start), length_(length){};
+    Range(int64_t start, int64_t length) : start_(start), length_(length){};
 
-    uint32_t getStart() { return start_; }
-    uint32_t getEnd() { return start_ + length_ - 1; }
-    uint32_t getLength() { return length_; }
+    int64_t getStart() { return start_; }
+    int64_t getEnd() { return start_ + length_ - 1; }
+    int64_t getLength() { return length_; }
 
 private:
-    uint32_t start_;
-    uint32_t length_;
+    int64_t start_;
+    int64_t length_;
 };
 
 class RangeMap
 {
 public:
-    RangeMap(uint32_t dest_start, uint32_t source_start, uint32_t length) : dest_start_(dest_start), source_start_(source_start), length_(length), source_end_(source_start + length - 1), shift_(dest_start - source_start) {}
+    RangeMap(int64_t source_start, int64_t source_end, int64_t shift) : source_start_(source_start), source_end_(source_end), shift_(shift) {}
 
-    bool inRange(uint32_t source)
+    bool inRange(int64_t source)
     {
-        return source >= source_start_ && source < source_start_ + length_;
+        return source >= source_start_ && source <= source_end_;
     }
 
-    uint32_t getDestination(uint32_t source)
+    int64_t getDestination(int64_t source)
     {
-        uint32_t offset = source - source_start_;
-        uint32_t destination = dest_start_ + offset;
-
-        return destination;
+        return source + shift_;
     }
 
-    uint32_t getDestStart() { return dest_start_; }
-    uint32_t getSourceStart() { return source_start_; }
-    uint32_t getDestEnd() { return dest_start_ + length_ - 1; }
-    uint32_t getSourceEnd() { return source_end_; }
-    uint32_t getLength() { return length_; }
+    int64_t getDestStart() { return source_start_ + shift_; }
+    int64_t getSourceStart() { return source_start_; }
+    int64_t getDestEnd() { return source_end_ + shift_; }
+    int64_t getSourceEnd() { return source_end_; }
+    int64_t getLength() { return source_end_ - source_start_; }
     int64_t getShift() { return shift_; }
 
 private:
-    uint32_t dest_start_;
-    uint32_t source_start_;
-    uint32_t length_;
-    uint32_t source_end_;
+    int64_t source_start_;
+    int64_t source_end_;
     int64_t shift_;
 };
 
@@ -207,7 +202,7 @@ public:
         uint8_t line_nr = 0;
         bool parsing_map = false;
         std::string map_name;
-        std::vector<uint32_t> seeds = {};
+        std::vector<int64_t> seeds = {};
         std::vector<RangeMap> current_map = {};
         std::map<std::string, std::vector<RangeMap>> maps;
         std::vector<std::string> map_order = {};
@@ -223,11 +218,11 @@ public:
                 std::stringstream seedLineStream(line.substr(7, line.size() - 7));
                 for (std::string seed; std::getline(seedLineStream, seed, ' ');)
                 {
-                    seeds.push_back(static_cast<uint32_t>(std::stoul(seed)));
+                    seeds.push_back(static_cast<int64_t>(std::stoul(seed)));
 
                     if (seed_nr % 2 == 1)
                     {
-                        Range seedRange(static_cast<uint32_t>(std::stoul(prev_seed)), static_cast<uint32_t>(std::stoul(seed)));
+                        Range seedRange(static_cast<int64_t>(std::stoul(prev_seed)), static_cast<int64_t>(std::stoul(seed)));
                         ranges.push_back(seedRange);
                     }
 
@@ -251,17 +246,17 @@ public:
             else if (parsing_map)
             {
                 std::string dest_start_str, source_start_str, length_str;
-                uint32_t dest_start, source_start, length;
+                int64_t dest_start, source_start, length;
                 std::stringstream rangeMapStream(line);
                 std::getline(rangeMapStream, dest_start_str, ' ');
                 std::getline(rangeMapStream, source_start_str, ' ');
                 std::getline(rangeMapStream, length_str, ' ');
 
-                dest_start = static_cast<uint32_t>(std::stoul(dest_start_str));
-                source_start = static_cast<uint32_t>(std::stoul(source_start_str));
-                length = static_cast<uint32_t>(std::stoul(length_str));
+                dest_start = static_cast<int64_t>(std::stoul(dest_start_str));
+                source_start = static_cast<int64_t>(std::stoul(source_start_str));
+                length = static_cast<int64_t>(std::stoul(length_str));
 
-                maps[map_name].push_back(*new RangeMap(dest_start, source_start, length));
+                maps[map_name].push_back(*new RangeMap(source_start, source_start + length, dest_start - source_start));
             }
 
             line_nr++;
@@ -273,13 +268,13 @@ public:
         map_order_ = map_order;
     }
 
-    std::vector<uint32_t> getSeeds() { return seeds_; }
+    std::vector<int64_t> getSeeds() { return seeds_; }
     std::vector<Range> getSeedRanges() { return ranges_; }
     std::map<std::string, std::vector<RangeMap>> getMaps() { return maps_; }
     std::vector<std::string> getMapOrder() { return map_order_; }
 
 private:
-    std::vector<uint32_t> seeds_;
+    std::vector<int64_t> seeds_;
     std::vector<Range> ranges_;
     std::map<std::string, std::vector<RangeMap>> maps_;
     std::vector<std::string> map_order_;
@@ -290,10 +285,40 @@ std::vector<RangeMap> transform_map(std::vector<RangeMap> input_map, std::vector
     std::vector<RangeMap> output_map;
     for (RangeMap iMap : input_map)
     {
-        for (RangeMap tMap : transformation_map)
-        {
+        std::vector<RangeMap> remaining_map = {iMap};
+        int64_t i = 0;
+        while (i < remaining_map.size()) {
+            for (RangeMap rMap : remaining_map) {
+                std::cout << rMap.getSourceStart() << "-" << rMap.getSourceEnd()
+                << " --> " << rMap.getDestStart() << "-" << rMap.getDestEnd() << std::endl;
+            }
+            for (RangeMap tMap : transformation_map)
+            {
+                if (iMap.getDestStart() >= tMap.getSourceStart() && iMap.getDestEnd() <= tMap.getSourceEnd()) {
+                    output_map.push_back(*new RangeMap(iMap.getSourceStart(), iMap.getSourceEnd(), iMap.getShift() + tMap.getShift()));
+                } else if (iMap.getDestStart() < tMap.getSourceStart() && iMap.getDestEnd() <= tMap.getSourceEnd()) {
+                    int64_t unmapped_start = tMap.getSourceStart() - iMap.getDestStart();
+                    output_map.push_back(*new RangeMap(iMap.getSourceStart() + unmapped_start, iMap.getSourceEnd(), iMap.getShift() + tMap.getShift()));
+                    remaining_map.push_back(*new RangeMap(iMap.getSourceStart(), iMap.getSourceStart() + unmapped_start, iMap.getShift()));
+                    i++;
+                } else if (iMap.getDestStart() >= tMap.getSourceStart() && iMap.getDestEnd() > tMap.getSourceEnd()) {
+                    int64_t unmapped_end = iMap.getDestEnd() - tMap.getSourceEnd();
+                    output_map.push_back(*new RangeMap(iMap.getSourceStart(), iMap.getSourceEnd() - unmapped_end, iMap.getShift() + tMap.getShift()));
+                    remaining_map.push_back(*new RangeMap(iMap.getSourceEnd() - unmapped_end, iMap.getSourceEnd(), iMap.getShift()));
+                    i++;
+                } else if (iMap.getDestStart() < tMap.getSourceStart() && iMap.getDestEnd() > tMap.getSourceEnd()) {
+                    int64_t unmapped_start = tMap.getSourceStart() - iMap.getDestStart();
+                    int64_t unmapped_end = iMap.getDestEnd() - tMap.getSourceEnd();
+                    output_map.push_back(*new RangeMap(iMap.getSourceStart() + unmapped_start, iMap.getSourceEnd() - unmapped_end, iMap.getShift() + tMap.getShift()));
+                    remaining_map.push_back(*new RangeMap(iMap.getSourceStart(), iMap.getSourceStart() + unmapped_start, iMap.getShift()));
+                    remaining_map.push_back(*new RangeMap(iMap.getSourceEnd() - unmapped_end, iMap.getSourceEnd(), iMap.getShift()));
+                    i += 2;
                 }
+            }
+        }
     }
+
+    return output_map;
 }
 
 int main()
@@ -302,37 +327,45 @@ int main()
     Almanac almanac(file);
 
     std::map<std::string, std::vector<RangeMap>> maps = almanac.getMaps();
-    uint32_t value;
-    std::vector<uint32_t> locations_1 = {};
-    std::vector<uint32_t> locations_2 = {};
 
-    for (const uint32_t seed : almanac.getSeeds())
+    std::vector<RangeMap> map_12 = transform_map(maps["seed-to-soil"], maps["soil-to-fertilizer"]);
+    for (RangeMap rangeMap : map_12)
     {
-        value = seed;
-        for (const auto map_name : almanac.getMapOrder())
-        {
-            std::sort(maps[map_name].begin(), maps[map_name].end(), [](RangeMap map1, RangeMap map2)
-                      { return map1.getSourceStart() < map2.getSourceStart(); });
-            std::vector<RangeMap> map = maps[map_name];
-
-            for (RangeMap rangeMap : map)
-            {
-                if (rangeMap.inRange(value))
-                {
-                    value = rangeMap.getDestination(value);
-                    break;
-                }
-            }
-        }
-        locations_1.push_back(value);
+        std::cout << rangeMap.getSourceStart() << "-" << rangeMap.getSourceEnd()
+        << " --> " << rangeMap.getDestStart() << "-" << rangeMap.getDestEnd() << std::endl;
     }
 
-    for (Range seedRange : almanac.getSeedRanges())
-    {
-        std::cout << "Seed range: " << seedRange.getStart() << " - " << seedRange.getEnd() << std::endl;
-    }
+    // int64_t value;
+    // std::vector<int64_t> locations_1 = {};
+    // std::vector<int64_t> locations_2 = {};
 
-    std::cout << "Answer part 1: " << *std::min_element(locations_1.begin(), locations_1.end()) << std::endl;
-    std::cout << "Answer part 2: " << *std::min_element(locations_2.begin(), locations_2.end()) << std::endl;
+    // for (const int64_t seed : almanac.getSeeds())
+    // {
+    //     value = seed;
+    //     for (const auto map_name : almanac.getMapOrder())
+    //     {
+    //         std::sort(maps[map_name].begin(), maps[map_name].end(), [](RangeMap map1, RangeMap map2)
+    //                   { return map1.getSourceStart() < map2.getSourceStart(); });
+    //         std::vector<RangeMap> map = maps[map_name];
+
+    //         for (RangeMap rangeMap : map)
+    //         {
+    //             if (rangeMap.inRange(value))
+    //             {
+    //                 value = rangeMap.getDestination(value);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     locations_1.push_back(value);
+    // }
+
+    // for (Range seedRange : almanac.getSeedRanges())
+    // {
+    //     std::cout << "Seed range: " << seedRange.getStart() << " - " << seedRange.getEnd() << std::endl;
+    // }
+
+    // std::cout << "Answer part 1: " << *std::min_element(locations_1.begin(), locations_1.end()) << std::endl;
+    // std::cout << "Answer part 2: " << *std::min_element(locations_2.begin(), locations_2.end()) << std::endl;
     return 0;
 }
